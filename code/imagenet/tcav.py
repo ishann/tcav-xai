6bad791c741946eb8cdcc3fda265440f39543448
Kenneth Mitra <kennethmitra@gmail.com>
2022-11-23 21:39:01 -0600

    Automatically generate hierarchy concepts and random concepts. Set num_images in assemble_multi_concept

--- code/imagenet/tcav.py
+++ code/imagenet/tcav.py
@@ -1,111 +1,145 @@
-import ipdb
-
-import numpy as np
-import os, glob
-
-import matplotlib.pyplot as plt
-
-from PIL import Image
-
-from scipy.stats import ttest_ind
+# import ipdb
 
 # ..........torch imports............
-import torch
 import torchvision
-
-from torch.utils.data import IterableDataset, DataLoader
-from torchvision import transforms
-
-#.... Captum imports..................
-from captum.attr import LayerGradientXActivation, LayerIntegratedGradients
-
+# .... Captum imports..................
+from captum.attr import LayerIntegratedGradients
 from captum.concept import TCAV
-from captum.concept import Concept
 
-from captum.concept._utils.data_iterator import dataset_to_dataloader, CustomIterableDataset
-from captum.concept._utils.common import concepts_to_str
+# .... Local imports..................
+from generate_data.hierarchy import Hierarchy
+from utils import assemble_all_concepts_from_hierarchy, assemble_random_concepts, generate_experiments
+from utils import load_image_tensors
 
-#.... Local imports..................
-from utils import transform, get_tensor_from_filename,load_image_tensors
-from utils import assemble_concept, assemble_multi_concept
+# Load Hierarchy
+HIERARCHY_JSON_PATH = 'generate_data/hierarchy.json'
+HIERARCHY_WORDNET_PATH = 'generate_data/wordnet_labels.txt'
+IMAGENET_IDX_TO_LABELS = 'generate_data/imagenet1000_clsidx_to_labels.txt'
+h = Hierarchy(json_path=HIERARCHY_JSON_PATH, wordnet_labels_path=HIERARCHY_WORDNET_PATH,
+              imagenet_idx_labels_path=IMAGENET_IDX_TO_LABELS)
 
-ROOT_PATH = "/home/devvrit/ishann/data/captum/tcav/concepts"
-
+###################################################
+# Assemble Concepts
 # Let's assemble concepts into Concept instances using Concept class and concept images stored in `concepts_path`.
-#ipdb.set_trace()
+###################################################
 
-concepts_path = ROOT_PATH
+# CONCEPTS_PATH = "/home/devvrit/ishann/data/captum/tcav/concepts"
+concepts_path = "../data"
 
-multi_concept = "canine"
-concepts = ["siberean_husky", "white_wolf"]
-canine_concept = assemble_multi_concept(multi_concept, concepts, 0, concepts_path=concepts_path)
+# Assemble non-random concepts
+concepts = assemble_all_concepts_from_hierarchy(h=h, num_images=100, concepts_path=concepts_path,
+                                                recreate_if_exists=True)  # Only 100 images for testing, can increase later
 
-#stripes_concept = assemble_concept("striped", 0, concepts_path=concepts_path)
-#zigzagged_concept = assemble_concept("zigzagged", 1, concepts_path=concepts_path)
-#dotted_concept = assemble_concept("dotted", 2, concepts_path=concepts_path)
+# Assemble all random concepts
+random_concepts = assemble_random_concepts(concepts_path=concepts_path)
 
-random_0_concept = assemble_concept("random_0", 1, concepts_path=concepts_path)
-random_1_concept = assemble_concept("random_1", 2, concepts_path=concepts_path)
+##############################
+# Experiments
+##############################
 
-# ## Defining GoogleNet Model
-
-# For this tutorial, we will load GoogleNet model and set in eval mode.
+# Defining GoogleNet Model
 model = torchvision.models.googlenet(pretrained=True)
 model = model.eval()
-
-# # Computing TCAV Scores
-
-# Next, let's create TCAV class by passing the instance of GoogleNet model, a custom classifier and the list of layers where we would like to test the importance of concepts.
-
-# The custom classifier will be trained to learn classification boundaries between concepts. We offer a default implementation of Custom Classifier in captum library so that the users do not need to define it. Captum users, hoowever, are welcome to define their own classifers with any custom logic. CustomClassifier class extends abstract Classifier class and provides implementations for training workflow for the classifier and means to access trained weights and classes. Typically, this can be, but is not limited to a classier, from sklearn library. In this case CustomClassifier wraps `linear_model.SGDClassifier` algorithm from sklearn library.
-
 # model.inception5b is the last inception module.
 # model.fc is the fully-connected layer outputting a 1000-way embedding.
-layers=['inception5b', 'fc']
+layers = ['inception5b', 'fc']
 
-#ipdb.set_trace()
-mytcav = TCAV(model=model,
-              layers=layers,
-              layer_attr_method = LayerIntegratedGradients(
-                model, None, multiply_by_inputs=False))
+# Generate experiment sets
+experiments = generate_experiments(concepts_dict=concepts, random_concepts_dict=random_concepts)
+print(experiments)
 
+# Create TCAV for each experiment
+tcav = TCAV(model=model, layers=layers,
+            layer_attr_method=LayerIntegratedGradients(model, None, multiply_by_inputs=False))
 
-# ##### Defining experimantal sets for CAV
+# Load sample images
+husky_images = load_image_tensors('Siberian husky', root_path=concepts_path, transform=False, count=100)
+husky_idx = h.imagenet_label2idx['Siberian husky']
 
-# Then, lets create 2 experimental sets: ["striped", "random_0"] and ["striped", "random_1"].
+valley_images = load_image_tensors('valley', root_path=concepts_path, transform=False, count=100)
+valley_idx = h.imagenet_label2idx['valley']
+
+tabby_images = load_image_tensors('tabby', root_path=concepts_path, transform=False, count=100)
+tabby_idx = h.imagenet_label2idx['tabby']
+
+# Run tests to get TCAV scores
+tcav_scores_husky = tcav.interpret(inputs=husky_images, experimental_sets=experiments, target=husky_idx, n_steps=5)
+print("tcav husky")
+print(tcav_scores_husky)
+
+tcav_scores_valley = tcav.interpret(inputs=valley_images, experimental_sets=experiments, target=valley_idx, n_steps=5)
+tcav_scores_tabby = tcav.interpret(inputs=tabby_images, experimental_sets=experiments, target=tabby_idx, n_steps=5)
+
+# multi_concept = "canine"
+# # concepts = ["siberean_husky", "white_wolf"]
+# concepts = ["Siberian husky", "white wolf"]
+# canine_concept = assemble_multi_concept(multi_concept, concepts, 0, concepts_path=concepts_path)
 #
-# We will train a classifier for each experimal set that learns hyperplanes which separate the concepts in each experimental set from one another. This is especially interesting when later we want to perform two-sided statistical significance testing in order to confirm or reject the significance of a concept in a specific layer for predictions.
+# #stripes_concept = assemble_concept("striped", 0, concepts_path=concepts_path)
+# #zigzagged_concept = assemble_concept("zigzagged", 1, concepts_path=concepts_path)
+# #dotted_concept = assemble_concept("dotted", 2, concepts_path=concepts_path)
 #
-# Now, let's load sample images from imagenet. The goal is to test how sensitive model predictions are to predefined concepts such as 'striped' when predicting 'zebra' class.
-experimental_set_rand = [[canine_concept, random_0_concept], [canine_concept, random_1_concept]]
-
-
-# Now, let's load sample images from imagenet. The goal is to test how sensitive model predictions are to predefined concepts such as 'striped' when predicting 'zebra' class.
+# random_0_concept = assemble_concept("random_0", 1, concepts_path=concepts_path)
+# random_1_concept = assemble_concept("random_1", 2, concepts_path=concepts_path)
 #
-# Please, download zebra images and place them under `data/tcav/image/imagenet/zebra` folder in advance before running the cell below. For our experiments we used 50 different zebra images from imagenet dataset.
-
-
-# Load sample images from folder
-husky_imgs = load_image_tensors('siberean_husky', root_path=ROOT_PATH, transform=False)
-
-# Here we perform a transformation and convert the images into tensors, so that we can use them as inputs to NN model.
-
-# Load sample images from folder
-husky_tensors = torch.stack([transform(img) for img in husky_imgs])
-experimental_set_rand
-
-
-# zebra class index
-# siberean_husky = 250
-# white_wolf = 270
-canine_ind = 250
-
-tcav_scores_w_random = mytcav.interpret(inputs=husky_tensors,
-                                        experimental_sets=experimental_set_rand,
-                                        target=canine_ind,
-                                        n_steps=5,
-                                       )
-
-tcav_scores_w_random
-
-
+# # ## Defining GoogleNet Model
+#
+# # For this tutorial, we will load GoogleNet model and set in eval mode.
+# model = torchvision.models.googlenet(pretrained=True)
+# model = model.eval()
+#
+# # # Computing TCAV Scores
+#
+# # Next, let's create TCAV class by passing the instance of GoogleNet model, a custom classifier and the list of layers where we would like to test the importance of concepts.
+#
+# # The custom classifier will be trained to learn classification boundaries between concepts. We offer a default implementation of Custom Classifier in captum library so that the users do not need to define it. Captum users, hoowever, are welcome to define their own classifers with any custom logic. CustomClassifier class extends abstract Classifier class and provides implementations for training workflow for the classifier and means to access trained weights and classes. Typically, this can be, but is not limited to a classier, from sklearn library. In this case CustomClassifier wraps `linear_model.SGDClassifier` algorithm from sklearn library.
+#
+# # model.inception5b is the last inception module.
+# # model.fc is the fully-connected layer outputting a 1000-way embedding.
+# layers=['inception5b', 'fc']
+#
+# #ipdb.set_trace()
+# mytcav = TCAV(model=model,
+#               layers=layers,
+#               layer_attr_method = LayerIntegratedGradients(
+#                 model, None, multiply_by_inputs=False))
+#
+#
+# # ##### Defining experimantal sets for CAV
+#
+# # Then, lets create 2 experimental sets: ["striped", "random_0"] and ["striped", "random_1"].
+# #
+# # We will train a classifier for each experimal set that learns hyperplanes which separate the concepts in each experimental set from one another. This is especially interesting when later we want to perform two-sided statistical significance testing in order to confirm or reject the significance of a concept in a specific layer for predictions.
+# #
+# # Now, let's load sample images from imagenet. The goal is to test how sensitive model predictions are to predefined concepts such as 'striped' when predicting 'zebra' class.
+# experimental_set_rand = [[canine_concept, random_0_concept], [canine_concept, random_1_concept]]
+#
+#
+# # Now, let's load sample images from imagenet. The goal is to test how sensitive model predictions are to predefined concepts such as 'striped' when predicting 'zebra' class.
+# #
+# # Please, download zebra images and place them under `data/tcav/image/imagenet/zebra` folder in advance before running the cell below. For our experiments we used 50 different zebra images from imagenet dataset.
+#
+#
+# # Load sample images from folder
+# # husky_imgs = load_image_tensors('siberean_husky', root_path=ROOT_PATH, transform=False)
+# husky_imgs = load_image_tensors('Siberian husky', root_path=concepts_path, transform=False)
+#
+# # Here we perform a transformation and convert the images into tensors, so that we can use them as inputs to NN model.
+#
+# # Load sample images from folder
+# husky_tensors = torch.stack([transform(img) for img in husky_imgs])
+# print(experimental_set_rand)
+#
+#
+# # zebra class index
+# # siberean_husky = 250
+# # white_wolf = 270
+# canine_ind = 250
+#
+# tcav_scores_w_random = mytcav.interpret(inputs=husky_tensors,
+#                                         experimental_sets=experimental_set_rand,
+#                                         target=canine_ind,
+#                                         n_steps=5,
+#                                        )
+#
+# print(tcav_scores_w_random)
--- code/imagenet/utils.py
+++ code/imagenet/utils.py
@@ -1,35 +1,30 @@
+import glob
+import os
+import random
+import re
+import shutil
+from pathlib import Path
+
 import numpy as np
-import os, glob, shutil
-
-import matplotlib.pyplot as plt
-
 from PIL import Image
+from captum.concept import Concept
+from captum.concept._utils.data_iterator import dataset_to_dataloader, CustomIterableDataset
+from torchvision import transforms
+from tqdm import tqdm
 
-from scipy.stats import ttest_ind
+from generate_data.hierarchy import Hierarchy
+
 
 # ..........torch imports............
-import torch
-import torchvision
+# .... Captum imports..................
 
-from torch.utils.data import IterableDataset, DataLoader
-from torchvision import transforms
 
-#.... Captum imports..................
-from captum.attr import LayerGradientXActivation, LayerIntegratedGradients
-
-from captum.concept import TCAV
-from captum.concept import Concept
-
-from captum.concept._utils.data_iterator import dataset_to_dataloader, CustomIterableDataset
-from captum.concept._utils.common import concepts_to_str
-
-import ipdb
+# import ipdb
 
 
 ### Defining image related transformations and functions
 # Method to normalize an image to Imagenet mean and standard deviation
 def transform(img):
-
     return transforms.Compose(
         [
             transforms.Resize(256),
@@ -48,15 +43,17 @@ def get_tensor_from_filename(filename):
     return transform(img)
 
 
-def load_image_tensors(class_name, root_path='data/tcav/image/imagenet/', transform=True):
-
-    #ipdb.set_trace()
+def load_image_tensors(class_name, root_path='data/tcav/image/imagenet/', transform=True, count=100):
+    # ipdb.set_trace()
 
     path = os.path.join(root_path, class_name)
     filenames = glob.glob(path + '/*.JPEG')
 
     tensors = []
-    for filename in filenames:
+    for i, filename in enumerate(filenames):
+        if i >= count:
+            break
+
         img = Image.open(filename).convert('RGB')
         tensors.append(transform(img) if transform else img)
 
@@ -73,18 +70,22 @@ def assemble_concept(name, id, concepts_path="data/tcav/image/concepts/"):
 
 
 # Creates a single multi-concept from a list of concepts.
-def assemble_multi_concept(multi_concept, concepts, id, concepts_path="/data/tcav/image/concepts/"):
+def assemble_multi_concept(name, concepts, id, num_images, concepts_path="/data/tcav/image/concepts/",
+                           recreate_if_exists=False):
     """
-    multi_concept (str): the general concept encapsulating all the concepts.
-                         Eg. "canine"."
+    name (str): the general concept encapsulating all the concepts.
+                     Eg. "canine"."
     concepts (list): list of all the concepts to be assembled.
                      Eg. ["siberean_husky", "white_wolf"].
     id: same as id from assemble_concept.
     concepts_path: same as concepts_path from assemble_concept.
+    num_images: number of images that the overall multi_concept should contain (drawn equally from each sub-concept)
+                IGNORED IF exists and recreate_if_exists is False
+    recreate_if_exists: behavior when image folder already exists
     """
 
     # Create a new directory for multi-concept.
-    multi_concept_path = os.path.join(concepts_path, multi_concept) + "/"
+    multi_concept_path = os.path.join(concepts_path, name) + "/"
     """
     import errno
     if not os.path.exists(multi_concept_path):
@@ -94,23 +95,102 @@ def assemble_multi_concept(multi_concept, concepts, id, concepts_path="/data/tca
             if e.errno != errno.EEXIST:
                 raise
     """
-    os.makedirs(multi_concept_path, exist_ok=True)
+    delete_old = Path(multi_concept_path).exists() and recreate_if_exists
+    copy_files = recreate_if_exists or not Path(multi_concept_path).exists()
 
-    # Copy all images from concepts_paths to multi_concept_path.
-    #concept_img_paths = []
-    for concept in concepts:
-        concept_path = os.path.join(concepts_path, concept)
-        concept_img_names = os.listdir(concept_path)
+    # Delete directory if exists and recreate
+    if delete_old:
+        assert len(os.listdir(multi_concept_path)) == len(
+            glob.glob(os.path.join(multi_concept_path, "*.JPEG"))), "Directory to delete must only contain images"
+        shutil.rmtree(Path(multi_concept_path), ignore_errors=True)
 
-        for concept_img_name in concept_img_names:
-            src = os.path.join(concept_path, concept_img_name)
-            dst = os.path.join(multi_concept_path, concept_img_name)
-            shutil.copyfile(src, dst)
+    if copy_files:
+        Path(multi_concept_path).mkdir(exist_ok=True)
 
-    #ipdb.set_trace()
+        # Copy all images from concepts_paths to multi_concept_path.
+        num_images_from_concepts = [len(arr) for arr in np.array_split(range(num_images), len(concepts))]
+
+        for concept, num_images_from_concept in zip(concepts, num_images_from_concepts):
+            concept_path = os.path.join(concepts_path, concept)
+            concept_img_names = os.listdir(concept_path)
+            random.shuffle(concept_img_names)  # Re-order list
+
+            assert num_images_from_concept <= len(
+                concept_img_names), "Number of images drawn from concept must be at most the number of available images"
+
+            for idx, concept_img_name in enumerate(concept_img_names):
+                src = os.path.join(concept_path, concept_img_name)
+                dst = os.path.join(multi_concept_path, concept_img_name)
+                shutil.copyfile(src, dst)
+
+                # Only copy desired amount
+                if idx + 1 >= num_images_from_concept:
+                    break
+
+    # ipdb.set_trace()
     dataset = CustomIterableDataset(get_tensor_from_filename, multi_concept_path)
     concept_iter = dataset_to_dataloader(dataset)
 
-    return Concept(id=id, name=multi_concept, data_iter=concept_iter)
+    return Concept(id=id, name=name, data_iter=concept_iter)
 
 
+def assemble_multi_concept_from_hierarchy(concept_name: str, h: Hierarchy, num_images: int, concepts_path: str,
+                                          recreate_if_exists: bool):
+    """
+    :param concept_name: Name of the concept (must be in hierarchy.json)
+    :param h: hierarchy object
+    :param recreate_if_exists: behavior if folder exists
+    :param concepts_path:
+    :param num_images: num images in non-leaf folders
+    """
+    node = h.find_node(concept_name)
+    assert node is not None, "Couldn't find concept name in hierarchy"
+
+    leaf_nodes = h.get_leaf_nodes(of=concept_name)
+    return assemble_multi_concept(name=concept_name, concepts=leaf_nodes, id=node['id'], num_images=num_images,
+                                  concepts_path=concepts_path, recreate_if_exists=recreate_if_exists)
+
+
+def assemble_all_concepts_from_hierarchy(h: Hierarchy, num_images: int, concepts_path: str, recreate_if_exists: bool):
+    """
+    Assemble all concepts in hierarchy
+    :param h: Hierarchy object
+    :param num_images: Number of images contained in each ***non-leaf*** concept
+    :param concepts_path: Directory where concept folders will be created and contains leaf node images
+    :return: dict mapping concept name to concept object
+    """
+
+    nodes = h.get_all_nodes()
+    leaf_nodes = set(h.get_leaf_nodes())
+
+    concepts = dict()
+
+    print("Assembling concepts...")
+    for node in tqdm(nodes, desc="Creating concepts"):
+        node_data = h.find_node(node)
+
+        if node in leaf_nodes:
+            concepts[node] = assemble_concept(name=node, id=node_data['id'], concepts_path=concepts_path)
+        else:
+            concepts[node] = assemble_multi_concept_from_hierarchy(concept_name=node, h=h, num_images=num_images,
+                                                                   concepts_path=concepts_path,
+                                                                   recreate_if_exists=recreate_if_exists)
+    return concepts
+
+
+def assemble_random_concepts(concepts_path: str):
+    random_concepts = dict()
+    random_dirs = glob.glob(os.path.join(concepts_path, "random_*", ""))
+
+    for random_dir in tqdm(random_dirs, desc="Creating Random concepts"):
+        idx = int(re.search(r"random_([\d]+)", random_dir).group(1))
+        random_concepts[idx] = assemble_concept(name=f"random_{idx}", id=2000 + idx, concepts_path=concepts_path)
+
+    return random_concepts
+
+
+def generate_experiments(concepts_dict: dict, random_concepts_dict: dict):
+    experiments = []
+    for i, concept in enumerate(concepts_dict):
+        experiments.append([concepts_dict[concept], random_concepts_dict[i]])
+    return experiments
